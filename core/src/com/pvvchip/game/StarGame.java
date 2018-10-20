d6db82faa493f2896620af53facf794d59e34a64
pvv <pvvchip@mail.ru>
2018-10-21 00:21:33 +0300

    move picture

--- core/src/com/pvvchip/game/StarGame.java
+++ core/src/com/pvvchip/game/StarGame.java
@@ -2,32 +2,89 @@ package com.pvvchip.game;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.InputProcessor;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Vector2;
 
-public class StarGame extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
-	
-	@Override
-	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
-	}
+public class StarGame extends ApplicationAdapter implements InputProcessor {
+    SpriteBatch batch;
+    Texture img;
 
-	@Override
-	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
-	}
-	
-	@Override
-	public void dispose () {
-		batch.dispose();
-		img.dispose();
-	}
+    private Vector2 pos;
+    private Vector2 v;
+    private int yMax, xMax;
+
+    @Override
+    public void create() {
+        Gdx.input.setInputProcessor(this);
+        yMax = Gdx.graphics.getHeight();
+        xMax = Gdx.graphics.getWidth();
+
+
+        batch = new SpriteBatch();
+        img = new Texture("badlogic.jpg");
+
+        pos = new Vector2(0, 0);
+        v = new Vector2(1, 0.5f);
+    }
+
+    @Override
+    public void render() {
+        Gdx.gl.glClearColor(0, 0.5f, 1, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        batch.begin();
+        batch.draw(img, pos.x, pos.y);
+        batch.end();
+        pos.add(v);
+    }
+
+    @Override
+    public void dispose() {
+        batch.dispose();
+        img.dispose();
+    }
+
+    @Override
+    public boolean keyDown(int keycode) {
+        System.out.println("Key: " + keycode);
+        return false;
+    }
+
+    @Override
+    public boolean keyUp(int keycode) {
+        return false;
+    }
+
+    @Override
+    public boolean keyTyped(char character) {
+        return false;
+    }
+
+    @Override
+    public boolean touchDown(int screenX, int screenY, int pointer, int button) {
+        System.out.println("X: " + screenX + " : Y: " + (yMax - screenY));
+        return false;
+    }
+
+    @Override
+    public boolean touchUp(int screenX, int screenY, int pointer, int button) {
+        return false;
+    }
+
+    @Override
+    public boolean touchDragged(int screenX, int screenY, int pointer) {
+        return false;
+    }
+
+    @Override
+    public boolean mouseMoved(int screenX, int screenY) {
+        return false;
+    }
+
+    @Override
+    public boolean scrolled(int amount) {
+        return false;
+    }
 }
