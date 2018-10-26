997053727d6282572028e1f87bf15fa07e9beb54
pvv <pvvchip@mail.ru>
2018-10-27 02:02:18 +0300

    lesson 3

--- desktop/src/com/pvvchip/game/desktop/DesktopLauncher.java
+++ desktop/src/com/pvvchip/game/desktop/DesktopLauncher.java
@@ -2,11 +2,11 @@ package com.pvvchip.game.desktop;
 
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
-import com.pvvchip.game.StarGame;
+import com.pvvchip.game.Star2DGame;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		new LwjglApplication(new StarGame(), config);
+		new LwjglApplication(new Star2DGame(), config);
 	}
 }
