3b93ec047480bb502fbc32a459724e10f2d556eb
pvv <pvvchip@mail.ru>
2018-11-17 23:22:42 +0300

    lesson 8

--- core/src/com/pvvchip/game/sprite/MessageGameOver.java
+++ core/src/com/pvvchip/game/sprite/MessageGameOver.java
@@ -3,9 +3,11 @@ package com.pvvchip.game.sprite;
 import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 import com.pvvchip.game.base.Sprite;
 
-public class GameOver extends Sprite {
-    public GameOver(TextureAtlas atlas) {
+public class MessageGameOver extends Sprite {
+
+    public MessageGameOver(TextureAtlas atlas) {
         super(atlas.findRegion("message_game_over"));
-        setHeightProportion(0.05f);
+        setHeightProportion(0.07f);
+        setBottom(0.009f);
     }
 }
